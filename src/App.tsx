8a7747ff9ddb41c06c20a0e5b7b143720612e7d3
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-01-08 07:37:37 +0500

    Todo Application Created

---
+++ src/App.tsx
@@ -0,0 +1,134 @@
+import { useState, useRef, useEffect } from "react";
+import { useCounterStore } from "./Store/Store";
+import { v4 as uuid } from "uuid";
+import gsap from "gsap";
+
+const App = () => {
+  const { addTodo, todos, deleteTodo, updateTodo } = useCounterStore();
+  const [text, setText] = useState("");
+  const [updatemode, setUpdateMode] = useState(false);
+  const [updatingTodoId, setUpdatingTodoId] = useState("");
+
+  const cardRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+
+  console.log(cardRefs.current);
+
+  useEffect(() => {
+    // Animate each card when it is added
+    const newTodoId = todos[todos.length - 1]?.id;
+
+    if (newTodoId && cardRefs.current[newTodoId]) {
+      const tl = gsap.timeline();
+      tl.fromTo(
+        cardRefs.current[newTodoId],
+        {
+          y: 50,
+          opacity: 0,
+          duration: 0.5,
+        },
+        {
+          y: 0,
+          opacity: 1,
+        }
+      );
+    }
+  }, [todos]);
+
+  const todoAddition = () => {
+    if (text.length > 0) {
+      if (updatemode) {
+        updateTodo(updatingTodoId, text);
+        setUpdateMode(false);
+        setUpdatingTodoId("");
+      } else {
+        const todo = { text, id: uuid() };
+        addTodo(todo);
+        setText("");
+      }
+    }
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    deleteTodo(id);
+  };
+
+  const handleUpdateTodo = (id: string, currentText: string) => {
+    setText(currentText);
+    setUpdateMode(true);
+    setUpdatingTodoId(id);
+  };
+
+  const reversedMap = todos.slice().reverse();
+
+  return (
+    <div className="min-h-screen bg-gray-600 flex justify-center items-center ">
+      <div className="h-[450px] w-[350px] bg-gray-800 rounded-md px-5">
+        <h1 className="font-bold font-sans text-white text-center mt-5 text-3xl">
+          Zustand
+        </h1>
+
+        <div className="flex justify-center mt-6">
+          <input
+            type="text"
+            className="w-full rounded-md outline-none px-3"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
+        </div>
+
+        <div className="flex justify-between mt-5">
+          {!updatemode ? (
+            <button
+              className="bg-red-500 py-[.5] text-white px-2 rounded-md"
+              onClick={todoAddition}
+            >
+              Create
+            </button>
+          ) : (
+            <button
+              className="bg-green-500 py-[.5] text-white px-2 rounded-md"
+              onClick={todoAddition}
+            >
+              Update
+            </button>
+          )}
+        </div>
+
+        <div className="mt-4 flex flex-col gap-5 h-[260px] py-3 rounded-md overflow-y-auto">
+          {reversedMap.map((value) => {
+            return (
+              <div
+                key={value.id}
+                className="flex justify-between bg-white px-2 min-h-[43px] items-center rounded-md card"
+                ref={(element) => {
+                  console.log(element);
+                  cardRefs.current[value.id] = element; //? element is representing the html element means the particular div on which the ref is attached.
+                }}
+              >
+                <h1 className="font-semibold">{value.text}</h1>
+
+                <div className="flex gap-2">
+                  <button
+                    className="bg-green-500 text-white px-2 rounded-md"
+                    onClick={() => handleUpdateTodo(value.id, value.text)}
+                  >
+                    ✏️
+                  </button>
+
+                  <button
+                    className="bg-red-500 text-white  px-2 rounded-md"
+                    onClick={() => handleDeleteTodo(value.id)}
+                  >
+                    🗑️
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Store/Store.tsx
@@ -0,0 +1,31 @@
+// Store/Store.ts
+import { create } from "zustand";
+import { v4 as uuid } from "uuid";
+
+type Todo = {
+  text: string;
+  id: string;
+};
+
+type CounterStore = {
+  todos: Todo[];
+  addTodo: (todo: Todo) => void;
+  deleteTodo: (id: string) => void;
+  updateTodo: (id: string, newText: string) => void;
+};
+
+export const useCounterStore = create<CounterStore>((set) => ({
+  todos: [{ text: "Buy a new book", id: uuid() }],
+  addTodo: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
+  deleteTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+
+  updateTodo: (id, newText) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      ),
+    })),
+}));
